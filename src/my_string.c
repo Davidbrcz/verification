d90741ec9fa0ae12a49fb495070cd51bd9751975
Davidbrcz <davidbrcz@gmail.com>
2015-01-04 16:15:20 +0100

    strappend : done

--- src/my_string.c
+++ src/my_string.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include "axiomatique.h"
 
-
 /*
   Return the number of caracters in the string (final zero excluded). 
 */
 /*@ requires \valid(str+ (0 .. strlen_(str))); 
   @ ensures \result == strlen_(str);
+  @ ensures \result >= 0;
   @ assigns \nothing;
   @*/
 int my_strlen(const char *str){
@@ -27,18 +27,18 @@ int my_strlen(const char *str){
 } 
 
 /*
-  Will copy length bytes starting at src+start in position  at dst
-
+  Will copy length bytes starting at src+start in position at dst
+  and add the zero terminator at dst[length]
 */
-/*@ requires length > 0;
+/*@ requires length >= 0;
   @ requires start >= 0;
   @ requires start+length <= strlen_(src);
   @ requires \valid(dst+(0..length));
-  @ requires \valid(src+(start..start+length)-1);
+  @ requires \valid(src+(start..start+length-1));
   @ requires \separated(dst+(0..length), (src+(start..start+length-1)));
   @ assigns dst[0..length];
-  @ ensures \forall integer jj;  0 <= jj < length ==> \at(dst[jj],Post)==\at(src[start+jj],Pre) ;
-  @ ensures \at(dst[length],Post)=='\0' ;
+  @ ensures copie :  \forall integer jj;  0 <= jj < length ==> \at(dst[jj],Post)==\at(src[start+jj],Pre) ;
+  @ ensures zero_terminated : \at(dst[length],Post)=='\0' ;
   @*/
 void strsubstring(char *dst, const char *src, int start, int length){
 
@@ -54,33 +54,19 @@ void strsubstring(char *dst, const char *src, int start, int length){
     /*@assert i==length;*/
     dst[length]='\0';
 }
-/*
-/* @ requires n > 0;
-  @ requires \valid(t+(0..(n-1)));
-  @ assigns t[0..n-1];
-  @ ensures \forall integer j;  0 <= j <  n ==> t[j] == x ;
-  @* /
-void fill(int* t, int n, int x) {
-    int i;
-    /* @ loop assigns i,t[0..(i-1)];     
-      @ loop invariant 0 <= i <= n;
-      @ loop invariant \forall integer j; 0 <= j < i ==> t[j] == x;
-      @ loop variant n - i;
-      @ * /
-  for (i = 0; i < n; i++)
-    t[i] = x;
 
-   return;
-}
-*/
-/*@ requires \valid(dst+(0..(strlen_(dst)+strlen_(src))));
-  @ requires \valid(src+(0..strlen_(src)));
-  @ ensures \forall integer j; 0<= j < strlen_(dst) ==> \at(dst[j],Old) == \at(dst[j],Pre);
-  @ ensures \forall integer j; strlen_(dst) <= j <= strlen_(dst)+strlen_(src) ==> \at(dst[j],Old) == \old(src[j]);
-@*/
+/*@ requires \valid(dst+(0..(strlen_{Pre}(dst)+strlen_{Pre}(src))));
+  @ requires \valid(src+(0..strlen_{Pre}(src)));
+  @ requires \separated(dst+(0..strlen_{Pre}(dst)+strlen_{Pre}(src)),src+(0..strlen_{Pre}(src)-1));
+  @ assigns dst[strlen_(dst)..strlen_(dst)+strlen_(src)];
+  @ ensures debut_constant : \forall integer j; 0<= j < \old(strlen_(dst)) ==> \at(dst[j],Old) == \at(dst[j],Post);
+  @ ensures suite_copiee : \forall integer j; 0 <= j < \old(strlen_(src)) ==> dst[j+\old(strlen_(dst))] == \at(src[j],Old);
+  @ ensures zero_final : dst[strlen_(dst)] == '\0' ;
+  @*/
 void strappend(char *dst, const char *src){
-//  strsubstring(dst+my_strlen(dst),src,0,my_strlen(src));
+    strsubstring(dst+my_strlen(dst),src,0,my_strlen(src));
 }
+
 /*
 int main(int argc, char *argv[])
 {
@@ -94,13 +80,14 @@ int main(int argc, char *argv[])
     strsubstring(dst,src,0,my_strlen(src)-5);
     printf ("|%s|\n",dst);
   }
+  printf("-------------------\n");
   {
     char dst[80]={'\0'}; 
     const char* str2 = "My Pipo";
     strncpy(dst,str,my_strlen(str));
-    strappend(dst,str2);
+    strappend2(dst,str2);
     printf ("|%s|\n",dst);
   }
   return 0;
-}
-*/
+}*/
+
